18e2146adc109d402d06f2cd3cd813e923f43bad
Facundo Martinez <martinez.facundo@globant.com>
2021-09-23 10:28:59 -0300

    Created clean architecture structure for the project
    Reduced minsdk to 23

--- app/src/main/java/com/onboarding/rick_and_morty_app/activity/MainActivity.kt
+++ app/src/main/java/com/onboarding/rick_and_morty_app/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.onboarding.rick_and_morty_app
+package com.onboarding.rick_and_morty_app.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.onboarding.rick_and_morty_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
